366b07272444952e819d7be04e23cf95add1542a
Ben Gamari <bgamari.foss@gmail.com>
2014-01-09 13:51:26 -0500

    test single and multiple open cases

--- examples/usb-serial-loopback/test.py
+++ examples/usb-serial-loopback/test.py
@@ -2,15 +2,31 @@
 
 import sys
 from serial import Serial
+from time import sleep
 
 test = 'hello world\n' 
-count = 0
-while True:
-        s = Serial('/dev/ttyACM0')
+def ping(s):
         s.write(test)
         r = s.readline()
         if r != test:
-                print r
-        sys.stdout.write('%d\r' % count)
+                #print 'uh oh:', test.encode('string_escape'), r.encode('string_escape')
+                pass
+        sys.stdout.write('.')
+        sys.stdout.flush()
+
+print 'single open: ',
+s = Serial('/dev/ttyACM0')
+for i in range(10):
+        ping(s)
+s.close()
+
+print
+print 'multiple open: ',
+count = 0
+while True:
+        s = Serial('/dev/ttyACM0')
+        ping(s)
+        #sys.stdout.write('%d\r' % count)
         count += 1
+        sleep(1)
         s.close()
